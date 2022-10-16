470e9a86c9b167ec2c75187a90c27b337df9472c
藤露 <080301087@163.com>
2022-10-16 23:01:34 +0800

    Create 03特征工程.py

---
+++ 01蒸汽量预测/03特征工程.py
@@ -0,0 +1,193 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+
+import warnings
+warnings.filterwarnings("ignore")
+
+train_data_file = "./zhengqi_train.txt"
+test_data_file =  "./zhengqi_test.txt"
+
+train_data = pd.read_csv(train_data_file, sep='\t', encoding='utf-8')
+test_data = pd.read_csv(test_data_file, sep='\t', encoding='utf-8')
+
+print(test_data.describe())
+
+# 箱型图做异常值检测
+plt.figure(figsize=(18, 10))
+plt.boxplot(x=train_data.values,labels=train_data.columns)
+plt.hlines([-7.5, 7.5], 0, 40, colors='r')
+plt.show()
+
+# 删除异常值
+train_data = train_data[train_data['V9']>-7.5]
+train_data.describe()
+
+test_data.describe()
+
+# 最大最小值归一化
+from sklearn import preprocessing
+
+features_columns = [col for col in train_data.columns if col not in ['target']]
+
+min_max_scaler = preprocessing.MinMaxScaler()
+
+min_max_scaler = min_max_scaler.fit(train_data[features_columns])
+
+train_data_scaler = min_max_scaler.transform(train_data[features_columns])
+test_data_scaler = min_max_scaler.transform(test_data[features_columns])
+
+train_data_scaler = pd.DataFrame(train_data_scaler)
+train_data_scaler.columns = features_columns
+
+test_data_scaler = pd.DataFrame(test_data_scaler)
+test_data_scaler.columns = features_columns
+
+train_data_scaler['target'] = train_data['target']
+
+# 查看所有特征变量下，训练集数据和测试集数据的分布情况，一共38个核密度估计子图
+# 可以看到特征'V5','V9','V11','V17','V22','V28' 训练集数据与测试集数据分布不一致，会导致模型泛化能力差，采用删除此类特征方法
+dist_cols = 5
+dist_rows = 8
+
+plt.figure(figsize=(4 * dist_cols, 2 * dist_rows))
+
+for i, col in enumerate(test_data_scaler.columns):
+    ax = plt.subplot(dist_rows, dist_cols, i + 1)
+    ax = sns.kdeplot(train_data_scaler[col], color="Red", shade=True)
+    ax = sns.kdeplot(test_data_scaler[col], color="Blue", shade=True)
+    ax.set_xlabel(col)
+    ax.set_ylabel("Frequency")
+    ax = ax.legend(["train", "test"])
+
+plt.show()
+
+# 查看特征'V5', 'V17', 'V28', 'V22', 'V11', 'V9'数据的数据分布
+drop_col = 6
+drop_row = 1
+
+plt.figure(figsize=(5*drop_col,5*drop_row))
+
+for i, col in enumerate(["V5","V9","V11","V17","V22","V28"]):
+    ax =plt.subplot(drop_row,drop_col,i+1)
+    ax = sns.kdeplot(train_data_scaler[col], color="Red", shade=True)
+    ax= sns.kdeplot(test_data_scaler[col], color="Blue", shade=True)
+    ax.set_xlabel(col)
+    ax.set_ylabel("Frequency")
+    ax = ax.legend(["train","test"])
+plt.show()
+
+# 特征相关性分布图
+plt.figure(figsize=(20, 16))
+column = train_data_scaler.columns.tolist()
+mcorr = train_data_scaler[column].corr(method="spearman")
+mask = np.zeros_like(mcorr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+g = sns.heatmap(mcorr, mask=mask, cmap=cmap, square=True, annot=True, fmt='0.2f')
+plt.show()
+
+# 相关性分析
+# 跟target相关性大于0.1的列倒序排序
+mcorr=mcorr.abs()
+numerical_corr=mcorr[mcorr['target']>0.1]['target']
+print(numerical_corr.sort_values(ascending=False))
+
+index0 = numerical_corr.sort_values(ascending=False).index
+print(train_data_scaler[index0].corr('spearman'))
+
+# 相关性初筛(单个特征之间的相关性)
+features_corr = numerical_corr.sort_values(ascending=False).reset_index()
+features_corr.columns = ['features_and_target', 'corr']
+features_corr_select = features_corr[features_corr['corr']>0.3] # 筛选出大于相关性大于0.3的特征
+print(features_corr_select)
+# 选出和target相关性大于0.3的特征
+select_features = [col for col in features_corr_select['features_and_target'] if col not in ['target']]
+new_train_data_corr_select = train_data_scaler[select_features+['target']] # 选中的列select_features加上target列组成的list
+new_test_data_corr_select = test_data_scaler[select_features]
+
+# 多重共线性分析
+from statsmodels.stats.outliers_influence import variance_inflation_factor #多重共线性方差膨胀因子
+
+#多重共线性(多个特征之间的相关性)
+# 神仔的视频讲VIF算法 https://www.bilibili.com/video/BV1R44y1z7Zc 第P4节
+# 张翔老师的视频讲VIF算法 https://www.youtube.com/watch?v=q_PjZOkPR90
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.outliers_influence.variance_inflation_factor.html
+new_numerical=['V0', 'V2', 'V3', 'V4', 'V5', 'V6', 'V10','V11',
+                         'V13', 'V15', 'V16', 'V18', 'V19', 'V20', 'V22','V24','V30', 'V31', 'V37']
+X=np.matrix(train_data_scaler[new_numerical])
+VIF_list=[variance_inflation_factor(X, i) for i in range(X.shape[1])] # i用来索引是X中的第几个变量的VIF值
+print('多重共线性的值')
+print(VIF_list)
+
+# PCA去除多重共线性 降维
+from sklearn.decomposition import PCA   #主成分分析法
+
+#PCA方法降维
+#保持90%的信息
+pca = PCA(n_components=0.9)
+new_train_pca_90 = pca.fit_transform(train_data_scaler.iloc[:,0:-1])
+new_test_pca_90 = pca.transform(test_data_scaler)
+new_train_pca_90 = pd.DataFrame(new_train_pca_90)
+new_test_pca_90 = pd.DataFrame(new_test_pca_90)
+new_train_pca_90['target'] = train_data_scaler['target']
+print(new_train_pca_90.describe())
+print('PCA方法降维 保持90%的信息')
+print(train_data_scaler.describe())
+
+# 再算一次多重共线性
+X=new_train_pca_90.iloc[:,0:-1].values # 等同于np.matrix(new_train_pca_90.iloc[:,0:-1])
+VIF_list=[variance_inflation_factor(X, i) for i in range(X.shape[1])] # i用来索引是X中的第几个变量的VIF值
+print('PCA方法降维后 多重共线性的值')
+print(VIF_list)
+
+
+#PCA方法降维
+#保留16个主成分
+pca = PCA(n_components=0.95)
+new_train_pca_16 = pca.fit_transform(train_data_scaler.iloc[:,0:-1])
+new_test_pca_16 = pca.transform(test_data_scaler)
+new_train_pca_16 = pd.DataFrame(new_train_pca_16)
+new_test_pca_16 = pd.DataFrame(new_test_pca_16)
+new_train_pca_16['target'] = train_data_scaler['target']
+new_train_pca_16.describe()
+print('PCA方法降维 保持95%的信息')
+print(train_data_scaler.describe())
+
+# 再算一次多重共线性
+X=new_train_pca_16.iloc[:,0:-1].values # 等同于np.matrix(new_train_pca_16.iloc[:,0:-1])
+VIF_list=[variance_inflation_factor(X, i) for i in range(X.shape[1])] # i用来索引是X中的第几个变量的VIF值
+print('PCA方法降维后 多重共线性的值')
+print(VIF_list)
+
+
+# 线性回归模型
+from sklearn.linear_model import LinearRegression  #线性回归
+from sklearn.neighbors import KNeighborsRegressor  #K近邻回归
+from sklearn.tree import DecisionTreeRegressor     #决策树回归
+from sklearn.ensemble import RandomForestRegressor #随机森林回归
+from sklearn.svm import SVR  #支持向量回归
+import lightgbm as lgb #lightGbm模型
+from sklearn.ensemble import GradientBoostingRegressor
+
+from sklearn.model_selection import train_test_split # 切分数据
+from sklearn.metrics import mean_squared_error #评价指标
+
+# 切分训练数据和线下验证数据
+#采用 pca 保留16维特征的数据
+new_train_pca_16 = new_train_pca_16.fillna(0)
+train = new_train_pca_16[new_test_pca_16.columns]
+target = new_train_pca_16['target']
+
+# 切分数据 训练数据80% 验证数据20%
+train_data,test_data,train_target,test_target=train_test_split(train,target,test_size=0.2,random_state=0)
+
+# 多元线性回归模型
+clf = LinearRegression()
+clf.fit(train_data, train_target)
+score = mean_squared_error(test_target, clf.predict(test_data))
+print("LinearRegression:   ", score)
+
