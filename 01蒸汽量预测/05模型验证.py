5311828048db3e6a0a14185e442007bc4b08961d
藤露 <080301087@163.com>
2022-10-17 00:37:19 +0800

    Update 05模型验证.py

--- 01蒸汽量预测/05模型验证.py
+++ 01蒸汽量预测/05模型验证.py
@@ -1,1 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+#过拟合与欠拟合
+#获取并绘制数据集
+np.random.seed(666)
+x = np.random.uniform(-3.0, 3.0, size=100)
+X = x.reshape(-1, 1)
+y = 0.5 * x**2 + x + 2 + np.random.normal(0, 1, size=100)
+
+plt.scatter(x, y)
+plt.show()
+
+# 1 使用线性回归拟合
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.score(X, y)# 输出：0.4953707811865009
+
+# 使用均方误差判断拟合程度
+from sklearn.metrics import mean_squared_error
+y_predict = lin_reg.predict(X)
+mean_squared_error(y, y_predict)# 输出：3.0750025765636577
+
+# 线性回归拟合的绘图结果
+y_predict = lin_reg.predict(X)
+plt.scatter(x, y)
+plt.plot(np.sort(x), y_predict[np.argsort(x)], color='r')
+plt.show()
+
+# 2 使用多项式回归拟合
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+
+def PolynomialRegression(degree):
+    return Pipeline([
+        ('poly', PolynomialFeatures(degree=degree)),
+        ('std_scaler', StandardScaler()),
+        ('lin_reg', LinearRegression())
+    ])
+
+# degree=2
+poly2_reg = PolynomialRegression(degree=2)
+poly2_reg.fit(X, y)
+
+y2_predict = poly2_reg.predict(X)
+
+# 比较真值和预测值的均方误差
+mean_squared_error(y, y2_predict)
+# 输出：1.0987392142417856
+# 画结果
+plt.scatter(x, y)
+plt.plot(np.sort(x), y2_predict[np.argsort(x)], color='r')
+plt.show()
+
+# degree=10
+poly10_reg = PolynomialRegression(degree=10)
+poly10_reg.fit(X, y)
+
+y10_predict = poly10_reg.predict(X)
+mean_squared_error(y, y10_predict)
+# 输出：1.0508466763764164
+# 画结果
+plt.scatter(x, y)
+plt.plot(np.sort(x), y10_predict[np.argsort(x)], color='r')
+plt.show()
+
+# 调整 degree = 100
+poly100_reg = PolynomialRegression(degree=100)
+poly100_reg.fit(X, y)
+
+y100_predict = poly100_reg.predict(X)
+mean_squared_error(y, y100_predict)
+# 输出：0.6874357783433694
+# 画结果
+plt.scatter(x, y)
+plt.plot(np.sort(x), y100_predict[np.argsort(x)], color='r')
+plt.show()
