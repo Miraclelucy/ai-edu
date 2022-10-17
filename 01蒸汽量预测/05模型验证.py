dc1981994ddae6e3fa16c38af836b533b221ca79
藤露 <080301087@163.com>
2022-10-18 00:02:08 +0800

    Update 05模型验证.py

--- 01蒸汽量预测/05模型验证.py
+++ 01蒸汽量预测/05模型验证.py
@@ -331,11 +331,12 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     plt.legend(loc="best")
     return plt
 
+# 下面绘制不同算法的学习曲线
 # 手写数字图片数据集
 digits = load_digits()
 X, y = digits.data, digits.target
 
-title = "Learning Curves (Naive Bayes)"  # 高斯朴素贝叶斯分类算法
+title = "Learning Curves (Naive Bayes)"  # 高斯朴素贝叶斯分类算法的学习曲线
 # Cross validation with 100 iterations to get smoother mean test and train
 # score curves, each time with 20% data randomly selected as a validation set.
 cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
@@ -343,11 +344,353 @@ estimator = GaussianNB() # 高斯朴素贝叶斯分类算法
 plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.01), cv=cv, n_jobs=4)
 plt.show()
 
-title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)" # 支持向量机分类算法
+title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)" # 支持向量机分类算法的学习曲线
 # SVC is more expensive so we do a lower number of CV iterations:
 cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
 estimator = SVC(gamma=0.001) # 支持向量机分类算法
 plot_learning_curve(estimator, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
 plt.show()
 
+# 验证曲线？？？
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+from sklearn. model_selection import validation_curve
+
+# 手写数字图片数据集
+digits = load_digits()
+X, y = digits.data, digits.target
+
+param_range = np.logspace(-6, -1, 5)
+train_scores, test_scores = validation_curve(
+    SVC(), X, y, param_name="gamma", param_range=param_range,
+    cv=10, scoring="accuracy", n_jobs=1)
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+plt.title("Validation Curve with SVM")
+plt.xlabel("$\gamma$")
+plt.ylabel("Score")
+plt.ylim(0.0, 1.1)
+plt.semilogx(param_range, train_scores_mean, label="Training score", color="r")
+plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                 train_scores_mean + train_scores_std, alpha=0.2, color="r")
+plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+             color="g")
+plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                 test_scores_mean + test_scores_std, alpha=0.2, color="g")
+plt.legend(loc="best")
+plt.show()
+
+# 模型过拟合与欠拟合
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.linear_model import LinearRegression  #线性回归
+from sklearn.neighbors import KNeighborsRegressor  #K近邻回归
+from sklearn.tree import DecisionTreeRegressor     #决策树回归
+from sklearn.ensemble import RandomForestRegressor #随机森林回归
+from sklearn.svm import SVR  #支持向量回归
+import lightgbm as lgb #lightGbm模型
+from sklearn.model_selection import train_test_split # 切分数据
+from sklearn.metrics import mean_squared_error #评价指标
+from sklearn.linear_model import SGDRegressor
+
+train_data_file = "./zhengqi_train.txt"
+test_data_file =  "./zhengqi_test.txt"
+
+train_data = pd.read_csv(train_data_file, sep='\t', encoding='utf-8')
+test_data = pd.read_csv(test_data_file, sep='\t', encoding='utf-8')
+
+
+# 归一化处理
+from sklearn import preprocessing
+
+features_columns = [col for col in train_data.columns if col not in ['target']]
+
+min_max_scaler = preprocessing.MinMaxScaler()
+
+min_max_scaler = min_max_scaler.fit(train_data[features_columns])
+
+train_data_scaler = min_max_scaler.transform(train_data[features_columns])
+test_data_scaler = min_max_scaler.transform(test_data[features_columns])
+
+train_data_scaler = pd.DataFrame(train_data_scaler)
+train_data_scaler.columns = features_columns
+
+test_data_scaler = pd.DataFrame(test_data_scaler)
+test_data_scaler.columns = features_columns
+
+train_data_scaler['target'] = train_data['target']
+
+
+# 用PAC进行降维处理
+from sklearn.decomposition import PCA   #主成分分析法
+
+#PCA方法降维
+#保留16个主成分
+pca = PCA(n_components=16)
+new_train_pca_16 = pca.fit_transform(train_data_scaler.iloc[:,0:-1])
+new_test_pca_16 = pca.transform(test_data_scaler)
+new_train_pca_16 = pd.DataFrame(new_train_pca_16)
+new_test_pca_16 = pd.DataFrame(new_test_pca_16)
+new_train_pca_16['target'] = train_data_scaler['target']
+
+#采用 pca 保留16维特征的数据
+new_train_pca_16 = new_train_pca_16.fillna(0)
+train = new_train_pca_16[new_test_pca_16.columns]
+target = new_train_pca_16['target']
+
+# 切分数据 训练数据80% 验证数据20%
+train_data,test_data,train_target,test_target=train_test_split(train,target,test_size=0.2,random_state=0)
+
+
+# 下面是模型训练
+# SGDRegressor是使用随机梯度下降进行线性回归的
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDRegressor.html
+# SGDRegressor
+clf = SGDRegressor(max_iter=500, tol=1e-2)
+clf.fit(train_data, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data))
+score_test = mean_squared_error(test_target, clf.predict(test_data))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 第一次优化 之前使用SGDRegressor会欠拟合，使用PolynomialFeatures构造多项式
+# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(5) # 构造5次多项式。如果是2次多项式的话，会包含1,a,b,ab,a^2,b^2一共6项
+train_data_poly = poly.fit_transform(train_data)
+test_data_poly = poly.transform(test_data)
+clf = SGDRegressor(max_iter=1000, tol=1e-3)
+clf.fit(train_data_poly, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data_poly))
+score_test = mean_squared_error(test_target, clf.predict(test_data_poly))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 第二次优化 5次多项式会过拟合，使用PolynomialFeatures构造多项式3次多项式，
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(3)
+train_data_poly = poly.fit_transform(train_data)
+test_data_poly = poly.transform(test_data)
+clf = SGDRegressor(max_iter=1000, tol=1e-3)
+clf.fit(train_data_poly, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data_poly))
+score_test = mean_squared_error(test_target, clf.predict(test_data_poly))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 第三次优化 L2范数正则化
+poly = PolynomialFeatures(3)
+train_data_poly = poly.fit_transform(train_data)
+test_data_poly = poly.transform(test_data)
+clf = SGDRegressor(max_iter=1000, tol=1e-3, penalty= 'L2', alpha=0.0001) # 就是这里的参数有改动
+clf.fit(train_data_poly, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data_poly))
+score_test = mean_squared_error(test_target, clf.predict(test_data_poly))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 第四次优化 L1范数正则化
+poly = PolynomialFeatures(3)
+train_data_poly = poly.fit_transform(train_data)
+test_data_poly = poly.transform(test_data)
+clf = SGDRegressor(max_iter=1000, tol=1e-3, penalty= 'L1', alpha=0.00001) # 就是这里的参数有改动
+clf.fit(train_data_poly, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data_poly))
+score_test = mean_squared_error(test_target, clf.predict(test_data_poly))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 第五次优化 ElasticNet L1和L2范数加权正则化
+poly = PolynomialFeatures(3)
+train_data_poly = poly.fit_transform(train_data)
+test_data_poly = poly.transform(test_data)
+clf = SGDRegressor(max_iter=1000, tol=1e-3, penalty= 'elasticnet', l1_ratio=0.9, alpha=0.00001) # 就是这里的参数有改动
+clf.fit(train_data_poly, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data_poly))
+score_test = mean_squared_error(test_target, clf.predict(test_data_poly))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+# 模型交叉验证
+# 简单交叉验证
+from sklearn.model_selection import train_test_split # 切分数据
+# 切分数据 训练数据80% 验证数据20%
+train_data,test_data,train_target,test_target=train_test_split(train,target,test_size=0.2,random_state=0)
+
+clf = SGDRegressor(max_iter=1000, tol=1e-3)
+clf.fit(train_data, train_target)
+score_train = mean_squared_error(train_target, clf.predict(train_data))
+score_test = mean_squared_error(test_target, clf.predict(test_data))
+print("SGDRegressor train MSE:   ", score_train)
+print("SGDRegressor test MSE:   ", score_test)
+
+
+# 5折交叉验证
+from sklearn.model_selection import KFold
+
+kf = KFold(n_splits=5)
+for k, (train_index, test_index) in enumerate(kf.split(train)):
+    train_data,test_data,train_target,test_target = train.values[train_index],train.values[test_index],target[train_index],target[test_index]
+    clf = SGDRegressor(max_iter=1000, tol=1e-3)
+    clf.fit(train_data, train_target)
+    score_train = mean_squared_error(train_target, clf.predict(train_data))
+    score_test = mean_squared_error(test_target, clf.predict(test_data))
+    print(k, " 折", "SGDRegressor train MSE:   ", score_train)
+    print(k, " 折", "SGDRegressor test MSE:   ", score_test, '\n')
+
+# 留一法 LOO CV
+from sklearn.model_selection import LeaveOneOut
+loo = LeaveOneOut()
+for k, (train_index, test_index) in enumerate(loo.split(train)):
+    train_data,test_data,train_target,test_target = train.values[train_index],train.values[test_index],target[train_index],target[test_index]
+    clf = SGDRegressor(max_iter=1000, tol=1e-3)
+    clf.fit(train_data, train_target)
+    score_train = mean_squared_error(train_target, clf.predict(train_data))
+    score_test = mean_squared_error(test_target, clf.predict(test_data))
+    print(k, " 个", "SGDRegressor train MSE:   ", score_train)
+    print(k, " 个", "SGDRegressor test MSE:   ", score_test, '\n')
+    if k >= 9:
+        break
+
+# 留P法 LPO CV
+from sklearn.model_selection import LeavePOut
+lpo = LeavePOut(p=10)
+num = 100
+for k, (train_index, test_index) in enumerate(lpo.split(train)):
+    train_data,test_data,train_target,test_target = train.values[train_index],train.values[test_index],target[train_index],target[test_index]
+    clf = SGDRegressor(max_iter=1000, tol=1e-3)
+    clf.fit(train_data, train_target)
+    score_train = mean_squared_error(train_target, clf.predict(train_data))
+    score_test = mean_squared_error(test_target, clf.predict(test_data))
+    print(k, " 10个", "SGDRegressor train MSE:   ", score_train)
+    print(k, " 10个", "SGDRegressor test MSE:   ", score_test, '\n')
+    if k >= 9:
+        break
+
+# 穷举网格搜索
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split # 切分数据
+# 切分数据 训练数据80% 验证数据20%
+train_data,test_data,train_target,test_target=train_test_split(train,target,test_size=0.2,random_state=0)
+
+randomForestRegressor = RandomForestRegressor()
+parameters = {
+              'n_estimators':[50, 100, 200],
+              'max_depth':[1, 2, 3]
+        }
+clf = GridSearchCV(randomForestRegressor, parameters, cv=5)
+clf.fit(train_data, train_target)
+score_test = mean_squared_error(test_target, clf.predict(test_data))
+print("RandomForestRegressor GridSearchCV test MSE:   ", score_test)
+sorted(clf.cv_results_.keys())
+
+# 随机参数优化
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split # 切分数据
+# 切分数据 训练数据80% 验证数据20%
+train_data,test_data,train_target,test_target=train_test_split(train,target,test_size=0.2,random_state=0)
+
+randomForestRegressor = RandomForestRegressor()
+parameters = {
+              'n_estimators':[50, 100, 200, 300],
+              'max_depth':[1, 2, 3, 4, 5]
+        }
+
+
+clf = RandomizedSearchCV(randomForestRegressor, parameters, cv=5)
+clf.fit(train_data, train_target)
+score_test = mean_squared_error(test_target, clf.predict(test_data))
+print("RandomForestRegressor RandomizedSearchCV test MSE:   ", score_test)
+sorted(clf.cv_results_.keys())
+
+# Lgb网格搜索 进行调参
+clf = lgb.LGBMRegressor(num_leaves=31)
+parameters = {
+    'learning_rate': [0.01, 0.1, 1],
+    'n_estimators': [20, 40]
+}
+clf = GridSearchCV(clf, parameters, cv=5)
+clf.fit(train_data, train_target)
+print('Best parameters found by grid search are:', clf.best_params_)
+score_test = mean_squared_error(test_target, clf.predict(test_data))
+print("LGBMRegressor RandomizedSearchCV test MSE:   ", score_test)
+
+
+# Lgb 线下验证
+train_data2 = pd.read_csv('./zhengqi_train.txt',sep='\t')
+test_data2 = pd.read_csv('./zhengqi_test.txt',sep='\t')
+
+train_data2_f = train_data2[test_data2.columns].values
+train_data2_target = train_data2['target'].values
+
+# lgb 模型
+from sklearn.model_selection import KFold
+import lightgbm as lgb
+import numpy as np
+
+# 5折交叉验证
+Folds = 5
+kf = KFold(n_splits=Folds, random_state=100, shuffle=True)
+# 记录训练和预测MSE
+MSE_DICT = {
+    'train_mse': [],
+    'test_mse': []
+}
+
+# 线下训练预测
+for i, (train_index, test_index) in enumerate(kf.split(train_data2_f)):
+    # lgb树模型
+    lgb_reg = lgb.LGBMRegressor(
+        learning_rate=0.01,
+        max_depth=-1,
+        n_estimators=100,
+        boosting_type='gbdt',
+        random_state=100,
+        objective='regression',
+    )
+
+    # 切分训练集和预测集
+    X_train_KFold, X_test_KFold = train_data2_f[train_index], train_data2_f[test_index]
+    y_train_KFold, y_test_KFold = train_data2_target[train_index], train_data2_target[test_index]
+
+    # 训练模型
+    #     reg.fit(X_train_KFold, y_train_KFold)
+    lgb_reg.fit(
+        X=X_train_KFold, y=y_train_KFold,
+        eval_set=[(X_train_KFold, y_train_KFold), (X_test_KFold, y_test_KFold)],
+        eval_names=['Train', 'Test'],
+        early_stopping_rounds=100,
+        eval_metric='MSE',
+        verbose=50
+    )
+
+    # 训练集预测 测试集预测
+    y_train_KFold_predict = lgb_reg.predict(X_train_KFold, num_iteration=lgb_reg.best_iteration_)
+    y_test_KFold_predict = lgb_reg.predict(X_test_KFold, num_iteration=lgb_reg.best_iteration_)
+
+    print('第{}折 训练和预测 训练MSE 预测MSE'.format(i))
+    train_mse = mean_squared_error(y_train_KFold_predict, y_train_KFold)
+    print('------\n', '训练MSE\n', train_mse, '\n------')
+    test_mse = mean_squared_error(y_test_KFold_predict, y_test_KFold)
+    print('------\n', '预测MSE\n', test_mse, '\n------\n')
+
+    MSE_DICT['train_mse'].append(train_mse)
+    MSE_DICT['test_mse'].append(test_mse)
+print('------\n', '训练MSE\n', MSE_DICT['train_mse'], '\n', np.mean(MSE_DICT['train_mse']), '\n------')
+print('------\n', '预测MSE\n', MSE_DICT['test_mse'], '\n', np.mean(MSE_DICT['test_mse']), '\n------')
 
