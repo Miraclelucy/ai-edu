2602c2bdcf970727fd0580c49b87d68b4ec5f678
藤露 <080301087@163.com>
2022-10-19 08:53:29 +0800

    Update 07模型融合.py

--- 01蒸汽量预测/07模型融合.py
+++ 01蒸汽量预测/07模型融合.py
@@ -159,3 +159,60 @@ rmse_scorer = make_scorer(rmse, greater_is_better=False)
 mse_scorer = make_scorer(mse, greater_is_better=False)
 
 # 获取异常数据，并画图
+# function to detect outliers based on the predictions of a model
+def find_outliers(model, X, y, sigma=3):
+
+    # predict y values using model
+    try:
+        y_pred = pd.Series(model.predict(X), index=y.index)
+    # if predicting fails, try fitting the model first
+    except:
+        model.fit(X,y)
+        y_pred = pd.Series(model.predict(X), index=y.index)
+        
+    # calculate residuals between the model prediction and true y values
+    resid = y - y_pred
+    mean_resid = resid.mean()
+    std_resid = resid.std()
+
+    # calculate z statistic, define outliers to be where |z|>sigma
+    z = (resid - mean_resid)/std_resid    
+    outliers = z[abs(z)>sigma].index
+    
+    # print and plot the results
+    print('R2=',model.score(X,y))
+    print('rmse=',rmse(y, y_pred))
+    print("mse=",mean_squared_error(y,y_pred))
+    print('---------------------------------------')
+
+    print('mean of residuals:',mean_resid)
+    print('std of residuals:',std_resid)
+    print('---------------------------------------')
+
+    print(len(outliers),'outliers:')
+    print(outliers.tolist())
+
+    plt.figure(figsize=(15,5))
+    ax_131 = plt.subplot(1,3,1)
+    plt.plot(y,y_pred,'.')
+    plt.plot(y.loc[outliers],y_pred.loc[outliers],'ro')
+    plt.legend(['Accepted','Outlier'])
+    plt.xlabel('y')
+    plt.ylabel('y_pred');
+
+    ax_132=plt.subplot(1,3,2)
+    plt.plot(y,y-y_pred,'.')
+    plt.plot(y.loc[outliers],y.loc[outliers]-y_pred.loc[outliers],'ro')
+    plt.legend(['Accepted','Outlier'])
+    plt.xlabel('y')
+    plt.ylabel('y - y_pred');
+
+    ax_133=plt.subplot(1,3,3)
+    z.plot.hist(bins=50,ax=ax_133)
+    z.loc[outliers].plot.hist(color='r',bins=50,ax=ax_133)
+    plt.legend(['Accepted','Outlier'])
+    plt.xlabel('z')
+    
+    plt.savefig('outliers.png')
+    
+    return outliers
